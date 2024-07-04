002332a757a03dbeed3bc14d98e6005a9f0388a2
Ben Samek <100028887@mvla.net>
2024-07-04 13:24:20 -0700

    fake commit

---
+++ src2/main/include/ClimbConstants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace ClimbIDs {
+    const int CLIMB_MOTOR=31;
+    const int CLIMB_CONTROLLER=2;
+    const int BRAKE=41;
+}
+namespace ClimbConstants{
+const double Total_Length= 22.0;//in inches
+const double Spindle_Radius= 0.5;//in inches 
+const double Pi=3.14;
+const double Max_Voltage= 2;//in volts
+const double Accel_Voltage_Max=0.002;//in volts/s
+}
